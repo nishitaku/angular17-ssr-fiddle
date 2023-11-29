f80ae0697015203050d31376408ccc0b87614ce3
nishitaku <29690066+nishitaku@users.noreply.github.com>
2023-11-29 22:53:13 +0900

    add api call sample

--- src/app/pages/foo/foo.component.ts
+++ src/app/pages/foo/foo.component.ts
@@ -1,13 +1,32 @@
-import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { CommonModule, JsonPipe } from '@angular/common';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit, inject, signal } from '@angular/core';
 
 @Component({
   selector: 'app-foo',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, JsonPipe],
   templateUrl: './foo.component.html',
-  styleUrl: './foo.component.css'
+  styleUrl: './foo.component.css',
 })
-export class FooComponent {
+export class FooComponent implements OnInit {
+  user = signal<any>(null);
 
+  http = inject(HttpClient);
+
+  ngOnInit() {
+    this.fetchUser();
+  }
+
+  getUser() {
+    this.fetchUser();
+  }
+
+  private fetchUser() {
+    this.http
+      .get('https://jsonplaceholder.typicode.com/users/1')
+      .subscribe((user) => {
+        this.user.set(user);
+      });
+  }
 }
