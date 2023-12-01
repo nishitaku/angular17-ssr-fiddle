c331c152b2679b206795b02eca67fdde9611262d
nishitaku <29690066+nishitaku@users.noreply.github.com>
2023-12-01 23:29:41 +0900

    root path redirect to /foo

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,6 +2,11 @@ import { Routes } from '@angular/router';
 
 export const routes: Routes = [
   {
+    path: '',
+    pathMatch: 'full',
+    redirectTo: '/foo',
+  },
+  {
     path: 'foo',
     loadComponent: () =>
       import('./pages/foo/foo.component').then((m) => m.FooComponent),
