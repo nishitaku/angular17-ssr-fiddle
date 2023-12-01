ae823b767244a5c057258a4d091bd63b1d816ca1
nishitaku <29690066+nishitaku@users.noreply.github.com>
2023-12-01 22:25:50 +0900

    fetch shibes at foo page

--- src/app/pages/foo/foo.component.ts
+++ src/app/pages/foo/foo.component.ts
@@ -1,25 +1,31 @@
-import { CommonModule, JsonPipe } from '@angular/common';
+import { CommonModule, JsonPipe, NgFor } from '@angular/common';
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit, inject, signal } from '@angular/core';
 
 @Component({
   selector: 'app-foo',
   standalone: true,
-  imports: [CommonModule, JsonPipe],
+  imports: [CommonModule, JsonPipe, NgFor],
   templateUrl: './foo.component.html',
   styleUrl: './foo.component.css',
 })
 export class FooComponent implements OnInit {
   user = signal<any>(null);
+  shibes = signal<any>(null);
 
   http = inject(HttpClient);
 
   ngOnInit() {
     this.fetchUser();
+    this.fetchShibes();
   }
 
   getUser() {
     this.fetchUser();
+  }
+
+  getShibes() {
+    this.fetchShibes();
   }
 
   private fetchUser() {
@@ -27,6 +33,14 @@ export class FooComponent implements OnInit {
       .get('https://jsonplaceholder.typicode.com/users/1')
       .subscribe((user) => {
         this.user.set(user);
+      });
+  }
+
+  private fetchShibes() {
+    this.http
+      .get('http://shibe.online/api/shibes?count=1&urls=true&httpsUrls=true')
+      .subscribe((shibes) => {
+        this.shibes.set(shibes);
       });
   }
 }
